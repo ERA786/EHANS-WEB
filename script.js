af9d2cff1926cf5c14612c21094944dae30d1a5f
ERA786 <killerxehanapex@gmail.com>
2025-04-02 23:08:15 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// This function opens the sidebar
+function openNav() {
+    document.getElementById("sidebar").style.width = "250px";
+}
+
+// This function closes the sidebar
+function closeNav() {
+    document.getElementById("sidebar").style.width = "0";
+}
